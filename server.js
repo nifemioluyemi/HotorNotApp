91866c668c984f92c1829d3fcde50dd8681b4473
Nifemi Oluyemi <nifemioluyemi@yahoo.com>
2018-09-07 21:41:40 -0400

    First Commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+let port = 3000;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
